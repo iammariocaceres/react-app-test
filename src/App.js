8325d913372a425c7530dc79af288bf54ccf8254
Mario Caceres <mcaceres@edarkstore.cl>
2022-03-04 11:03:17 -0300

    add console log

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log(process.env)
+  console.log(process.env.PRUEBA)
   return (
     <div className="App">
       <header className="App-header">
